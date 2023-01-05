029a50cc4f19a3faefaaec7e06b6d16cb1bfd007
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-05 00:40:50 -0800

    initial commit

---
+++ src/Contracts/Engine.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Engine interface
+ *
+ * @package   Backdrop
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2023. Benjamin Lu
+ * @link      https://github.com/benlumia007/backdrop-template-view
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+
+ */
+
+/**
+ * Define namespace
+ */
+namespace Backdrop\Template\View\Contracts;
+
+/**
+ * Engine interface.
+ * 
+ * @since  1.0.0
+ * @access public
+ */
+interface Engine {
+    /**
+     * Returns a View object.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string           $name
+     * @param  array|string     $slugs
+     * @param  array|Collection $data
+     * @return View
+     */
+    public function view( $name, $slugs = [], $data = [] );
+
+    /**
+     * Outputs a view template.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string           $name
+     * @param  array|string     $slugs
+     * @param  array|Collection $data
+     * @return void
+     */
+    public function display( $name, $slugs = [], $data = [] );
+
+    /**
+     * Returns a view template as a string.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string           $name
+     * @param  array|string     $slugs
+     * @param  array|Collection $data
+     * @return void
+     */
+    public function render( $name, $slugs = [], $data = [] );
+}
